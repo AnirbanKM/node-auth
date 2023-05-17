a21d152a50794cf04898665dc29fc32004038652
AnirbanKM <anirbankreative22@gmail.com>
2023-05-17 13:03:55 +0530

    cors config

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.listen(PORT, function () {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://mern-auth-01.onrender.com"],
     credentials: true
 }));
 
